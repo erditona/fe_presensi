852b978da89bc90660730fccebcc863d7df450a3
erditona <erditonaushaadam@gmail.com>
2023-03-31 15:58:53 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,7 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTable } from "./table.js";
+import { isiTable } from "./table";
 let urlAPI = "https://ws-dito.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
