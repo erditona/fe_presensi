afde9450f3ffa5db8bd458798a3d967a11b08d02
erditona <erditonaushaadam@gmail.com>
2023-03-31 15:26:37 +0700

    buat tabel

--- js/fetch.js
+++ js/fetch.js
@@ -8,3 +8,15 @@ function isiTablePresensi(results) {
 function isiRow(value) {
   console.log(value);
 }
+function isiRow(value) {
+  let content = isiTabel
+    .replace("#NAMA#", value.biodata.nama)
+    .replace("#NOHP#", value.biodata.phone_number)
+    .replace("#JABATAN#", value.biodata.jabatan)
+    .replace("#LOKASI#", value.location)
+    .replace("#STATUS#", value.checkin)
+    .replace("#HARIKERJA#", value.biodata.hari_kerja)
+    .replace("#WARNA#", getRandomColor())
+    .replace(/#WARNALOGO#/g, getRandomColorName());
+  addInner("iniTabel", content);
+}
