c0c8d162286ce91a50f17797b7d21dd10442571e
erditona <erditonaushaadam@gmail.com>
2023-03-31 15:35:52 +0700

    update fetch.js3

--- js/fetch.js
+++ js/fetch.js
@@ -1,14 +1,14 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "./temp/table.js";
+import { isiTable } from "./table";
 let urlAPI = "https://ws-dito.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
   results.forEach(isiRow);
 }
 function isiRow(value) {
-  let content = isiTabel
+  let content = isiTable
     .replace("#NAMA#", value.biodata.nama)
     .replace("#NOHP#", value.biodata.phone_number)
     .replace("#JABATAN#", value.biodata.jabatan)
