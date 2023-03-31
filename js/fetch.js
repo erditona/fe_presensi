5d3da577ca1a153f5b903bb6a972e996d7bd80ef
erditona <erditonaushaadam@gmail.com>
2023-03-31 15:18:51 +0700

    update-fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -3,4 +3,8 @@ let urlAPI = "https://ws-dito.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
   console.log(results);
+  results.forEach(isiRow);
+}
+function isiRow(value) {
+  console.log(value);
 }
