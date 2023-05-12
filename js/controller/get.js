b72c41b970606ec363d87f72f1a7fcd89ef7fd3b
erditona <erditonaushaadam@gmail.com>
2023-05-12 16:12:53 +0700

    update sort

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,10 +1,19 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTable } from "../temp/table.js";
+
+var Myvar = {};
+
 export function isiTablePresensi(results) {
+  Myvar.length = results.length;
+  results.sort((a, b) => new Date(b.datetime) - new Date(a.datetime)); //mengurutkan berdasasrkan datetime in descending order
   results.forEach(isiRow);
+  console.log(results);
 }
+
 function isiRow(value) {
+  document.getElementById("jml").textContent = Myvar.length + " Data";
+
   let content = isiTable
     .replace("#NAMA#", value.biodata.nama)
     .replace("#NOHP#", value.biodata.phone_number ? value.biodata.phone_number : value.phone_number ? value.phone_number : "#NOHP#")
