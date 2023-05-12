582ea7ef707a96eb4078509e9a9948b008a95113
erditona <erditonaushaadam@gmail.com>
2023-05-12 15:34:45 +0700

    push week9-latihanPost

---
+++ js/config/url_post.js
@@ -0,0 +1,7 @@
+export let urlPOST = "https://ws-dito.herokuapp.com/ins";
+
+export function AmbilResponse(result) {
+  console.log(result); //menampilkan response API pada console
+  alert(result.message); //menampilkan response API pada alert
+  window.location.reload(); //reload halaman setelah klik ok pada alert
+}
